104195f600b04e7a9d93c1c73737eeb60a6b1084
perblac <yhana.kromx@gmail.com>
2023-10-20 16:40:52 +0200

    ej2, solo implementado Class

--- ejercicios_prototipos/ej2/assets/modules.js
+++ ejercicios_prototipos/ej2/assets/modules.js
@@ -1,5 +1,35 @@
 // usando class:
-
+export class Comida {
+    _nombre;
+    _calorias;
+    constructor(nombre,calorias){
+        this._nombre = nombre;
+        this._calorias = calorias;
+    }
+    get nombre() {
+        return this._nombre;
+    }
+    calcularCalorias() {
+        return this._calorias;
+    }
+}
+export class Dieta {
+    _alimentosConsumidos;
+    constructor() {
+        this._alimentosConsumidos = [];
+    }
+    agregarAlimento(alimento) {
+        if (alimento instanceof Comida) {
+            this._alimentosConsumidos.push(alimento);
+        } else throw new Error("No comas cosas raras");
+    }
+    calcularTotalCalorias() {
+        return this._alimentosConsumidos.reduce((acum,alim) => acum + alim.calcularCalorias(), 0);
+    }
+    listarAlimentosConsumidos() {
+        return this._alimentosConsumidos.map((alim)=>`Alimento: ${alim.nombre} - Calorias: ${alim.calcularCalorias()}\n`).join('');
+    }
+}
 // usando función constructora:
 
 // usando objeto literal:
--- ejercicios_prototipos/ej2/script.js
+++ ejercicios_prototipos/ej2/script.js
@@ -1,8 +1,65 @@
 
 /* -------------------------------- importar -------------------------------- */
-
+import { Comida, Dieta } from "./assets/modules.js";
 /* --------------------------- declarar variables --------------------------- */
-
+// const dieta = new Dieta();
+// const tomate = new Comida('tomate', 50);
+// const queso = new Comida('queso', 100);
+// dieta.agregarAlimento(tomate);
+// dieta.agregarAlimento(queso);
+// dieta.listarAlimentosConsumidos(),
+// console.log("total:", dieta.calcularTotalCalorias());
+// console.log(typeof tomate);
+const alimentoFrm = document.getElementById('alimento');
+const caloriasFrm = document.getElementById('calorias')
+let dieta = null;
 /* -------------------------------- funciones ------------------------------- */
+function handlerCreateDiet(e) {
+    e.preventDefault();
+    dieta = new Dieta();
+    document.getElementById('formAlimentos').style.display = 'block';
+    console.log(dieta);
+    document.getElementById('crearDieta').setAttribute('disabled',1 );
+}
 
+function handlerEat(e) {
+    e.preventDefault();
+    const alimento = alimentoFrm.value;
+    const calorias = caloriasFrm.value;
+    if (alimento == '' ||
+        calorias == '') {
+        document.getElementById('mensajeExito').style.display = 'none';
+        document.getElementById('mensajeError').style.display = 'block';
+        setTimeout(()=> {
+            document.getElementById('mensajeError').style.value = 'none';
+        }, 5000);
+    } else {
+        let comida = new Comida(alimento,parseInt(calorias));
+        dieta.agregarAlimento(comida);
+        document.getElementById('mensajeError').style.display = 'none';
+        document.getElementById('mensajeExito').innerHTML = `¡Ñam! +1 ${comida.nombre}`;
+        document.getElementById('mensajeExito').style.display = 'block';
+        setTimeout(()=> {
+            document.getElementById('mensajeExito').style.value = 'none';
+        }, 2000);
+        alimentoFrm.value = "";
+        caloriasFrm.value = "";
+    }
+}
+
+function handlerCalculateCalories(e) {
+    e.preventDefault();
+
+    const area = document.getElementById('areaCalorias');
+    area.value = "";
+    area.value += dieta.listarAlimentosConsumidos();
+    area.value += `total: ${dieta.calcularTotalCalorias()}`;
+}
+
+function init() {
+    document.getElementById('crearDieta').addEventListener('click',handlerCreateDiet);
+    document.getElementById('comer').addEventListener('click', handlerEat);
+    document.getElementById('calcularCalorias').addEventListener('click', handlerCalculateCalories);
+}
 /* ---------------------------- INICIO APLICACION --------------------------- */
+document.addEventListener('DOMContentLoaded', init);
