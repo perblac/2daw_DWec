307752278d1a3e291bb076e0a1450496d513038d
perblac <yhana.kromx@gmail.com>
2023-10-21 22:54:23 +0200

    inicio ej3

---
+++ ejercicios_prototipos/ej3/assets/modules.js
@@ -0,0 +1,34 @@
+// usando class:
+class Vehiculo {
+    _marca;
+    _modelo;
+    _año;
+    constructor(marca, modelo, año) {
+        this._marca = marca;
+        this._modelo = modelo;
+        this._año = año;
+    }
+    acelerar() {}
+}
+
+class Coche extends Vehiculo {
+    arrancar(){
+
+    }
+    frenar(){
+
+    }
+}
+
+class Moto extends Vehiculo {
+    arrancar(){
+
+    }
+    frenar(){
+
+    }
+}
+
+// usando función constructora:
+
+// usando objeto literal:ç
